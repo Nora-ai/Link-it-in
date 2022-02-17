d320fd2ffcb1dd5f1cecc1b74b09bde687f8dab8
snora <noratulchina@gmail.com>
2022-02-17 15:16:50 +0200

    added delete request

--- client/src/screens/Job/Job.jsx
+++ client/src/screens/Job/Job.jsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { baseURL, config} from '../../services/apiConfig'
@@ -7,6 +7,7 @@ import './Job.css'
 export default function Job (props) {
     const [oneJob, setOneJob] = useState({})
     const { id } = useParams()
+    const navigate = useNavigate()
 
     useEffect(() => {
         const getOneJob = async () => {
@@ -19,8 +20,14 @@ export default function Job (props) {
 
     const { link, company, position, salary, status, techAss, appProcess, nextRound, final, notes } = oneJob
 
-    console.log(company)
-
+    
+    const handleDelete = async () => {
+        const response = await axios.delete(`${baseURL}/${id}`, config)
+        if (response) {
+            navigate('/jobs')
+        }
+    }
+ 
 
     return (<>
         <div className="job-container">
@@ -38,9 +45,10 @@ export default function Job (props) {
             <p>Next Round: {nextRound}</p>
             <p>Final: {final}</p>
             <p>Notes: {notes}</p>
+            <Link to={`/jobs/${id}/edit`}><button>Edit</button></Link>
+            <button onClick={handleDelete}>Delete</button>
             </div>
         </div>
-       <Link to={`/jobs/${id}/edit`}><button>Edit</button></Link>
 
     </>)
 }
